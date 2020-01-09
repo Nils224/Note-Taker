c30af1328ab47726caf57d1cfa9494edd8ce5497
Nilesh Patel <npatel224@gmail.com>
2020-01-09 18:07:26 +0000

    changed post to recieve json

--- server.js
+++ server.js
@@ -7,63 +7,65 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/assets/css",express.static(path.join(__dirname, "./public/assets/css")));
-app.use("/assets/js",express.static(path.join(__dirname, "./public/assets/js"))); 
+app.use("/assets/css",express.static(path.join(__dirname, "public/assets/css")));
+app.use("/assets/js",express.static(path.join(__dirname, "public/assets/js"))); 
 app.use(express.static(path.join(__dirname, "./public")));
 
 
 //HTML routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
 app.get("/index", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 
 app.get("/api/notes", (req, res)=> {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 
 //API routes
 app.post("/api/notes", (req, res) => {
 
-    const newNote = req.body;
+    let newNote = req.body;
     newNote.id = uuidv1();
 
     if (!newNote) {
         throw error
     }
 
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), (err, data) => {
         if (err) {
             throw err
         };
 
-        const file = JSON.parse(data);
+        let file = JSON.parse(data);
 
         file.push(newNote);
 
-        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(file), err => {
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(file), err => {
             if (err) throw err;
         });
+
+        res.json(newNote);
     });
 
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    console.log(newNote);
 });
 
 
 app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), (err, data) => {
         if (err) {
             throw err
         };
@@ -71,14 +73,16 @@ app.delete("/api/notes/:id", (req, res) => {
         const notefile = JSON.parse(data);
 
         const newFile = notefile.filter(note => note.id != req.params.id);
-        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newFile), err => {
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newFile), err => {
             if (err) {
                 throw err;
             }
         });
+        
+        res.sendFile(path.join(__dirname, "db/db.json"));
     });
 
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    
 });
 
 
